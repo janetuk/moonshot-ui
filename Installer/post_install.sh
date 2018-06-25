faf2eee5a416811f55412d5c3fa563ce6a675fba
Ivan Aleksovski <ivan@devsy.com>
2018-06-25 19:58:50 +0200

    Fixed installer to apply proper launchctl permissions.

--- Installer/post_install.sh
+++ Installer/post_install.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 rm /Applications/local.tar.gz
-
-sudo chmod 600 /usr/local/moonshot/org.freedesktop.dbus-session.plist
-sudo chown root /usr/local/moonshot/org.freedesktop.dbus-session.plist
-
-ln -sfv /usr/local/moonshot/*.plist ~/Library/LaunchAgents
-
-launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
-launchctl unload ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
-launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
