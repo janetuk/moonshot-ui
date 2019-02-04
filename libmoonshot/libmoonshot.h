e873bc01ee6962a4a3dc7654c04073209162c0fc
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2019-02-04 16:36:50 +0100

    Add a new DBUS method called install_id_card_2fa
    This methods allows to set the Has2FA value for an IdCard

--- libmoonshot/libmoonshot.h
+++ libmoonshot/libmoonshot.h
@@ -188,6 +188,50 @@ int moonshot_install_id_card (const char     *display_name,
                               MoonshotError **error);
 
 
+/**
+ * moonshot_install_id_card_2fa:
+ * @display_name: Display name of card
+ * @user_name: Username for identity, or %NULL
+ * @password: Password for identity, or %NULL
+ * @realm: Realm for identity, or %NULL
+ * @rules_patterns: Array of patterns for the service matching rules
+ * @rules_patterns_length: Length of @rules_patterns and @rules_always_confirm arrays
+ * @rules_always_confirm: Array of 'always confirm' flags corresponding to patterns
+ * @rules_always_confirm_length: Length of @rules_patterns and @rules_always_confirm arrays
+ * @services: Array of strings listing the services this identity provides
+ * @services_length: Length of @services array
+ * @ca_cert: The CA certificate, or %NULL
+ * @subject: Subject name constraint for @ca_cert, or %NULL
+ * @subject_alt: Subject alternative name constraint for @ca_cert, or %NULL
+ * @server_cert: Hash of the server certificate; required if @ca_cert is %NULL
+ * @error: Return location for a #MoonshotError.
+ *
+ * Calls the Moonshot server to add a new identity. The user will be prompted
+ * if they would like to add the ID card.
+ *
+ * The values for @rules_patterns_length and @rules_always_confirm_length should
+ * always be the same. They are present as separate parameters as a concession to
+ * the Vala bindings.
+ *
+ * Return value: %TRUE if the ID card was successfully added, %FALSE otherwise
+ */
+int moonshot_install_id_card_2fa (const char     *display_name,
+                                  const char     *user_name,
+                                  const char     *password,
+                                  const char     *realm,
+                                  char           *rules_patterns[],
+                                  int             rules_patterns_length,
+                                  char           *rules_always_confirm[],
+                                  int             rules_always_confirm_length,
+                                  char           *services[],
+                                  int             services_length,
+                                  const char     *ca_cert,
+                                  const char     *subject,
+                                  const char     *subject_alt,
+                                  const char     *server_cert,
+                                  int             force_flat_file_store,
+                                  int             has2fa,
+                                  MoonshotError **error);
 
 /**
  * moonshot_confirm_ca_certificate
