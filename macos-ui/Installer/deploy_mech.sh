dbc1d39f3da1a481a10cd0824cc95698fceb1d62
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2019-11-15 16:11:09 +0100

    Update library file names and locations
    Make every shipper library for MacOS lives on /usr/local/moonshot

--- macos-ui/Installer/deploy_mech.sh
+++ macos-ui/Installer/deploy_mech.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo tar -zxvf local.tar.gz -C /usr/local/
 sudo mkdir /etc/gss/
-printf "eap-aes128 1.3.6.1.5.5.15.1.1.17 /usr/local/lib/gss/mech_eap.so\neap-aes256 1.3.6.1.5.5.15.1.1.18 /usr/local/lib/gss/mech_eap.so" | sudo tee -a /etc/gss/mech
+printf "eap-aes128 1.3.6.1.5.5.15.1.1.17 /usr/local/moonshot/lib/gss/mech_eap.so\neap-aes256 1.3.6.1.5.5.15.1.1.18 /usr/local/moonshot/lib/gss/mech_eap.so" | sudo tee -a /etc/gss/mech
 
 sudo mkdir -p ~/Library/LaunchAgents
 sudo cp /usr/local/moonshot/org.freedesktop.dbus-session.plist ~/Library/LaunchAgents/
